4c64516368a07198af3681c9c91b23af3deb1e07
claire demont <claire.edith.demont@gmail.com>
2021-04-15 17:37:09 +0200

    genres, mood, movies crews models created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_121008) do
+ActiveRecord::Schema.define(version: 2021_04_15_132933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "crews", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "biography"
+    t.string "known_for"
+    t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "moods", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "en_title"
+    t.string "title"
+    t.string "language"
+    t.integer "year"
+    t.string "imdb_rating"
+    t.text "overview"
+    t.string "imdb_id"
+    t.string "poster_url"
+    t.bigint "crew_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["crew_id"], name: "index_movies_on_crew_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +66,5 @@ ActiveRecord::Schema.define(version: 2021_04_15_121008) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "movies", "crews"
 end
