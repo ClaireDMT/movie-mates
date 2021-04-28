1d31dcc06974d2625f0dbfcb07c1c1b54272d4d4
claire demont <claire.edith.demont@gmail.com>
2021-04-28 19:34:44 +0200

    set up the routes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import { Route, Switch} from 'react-router-dom'
+import Movie from './Movie/Movie'
 const App = () => {
   return (
-    <div>
-      hello world!
-    </div>
-  );
+    <Switch>
+      <Route exact path="/movies/:id" component={Movie} />
+      <Route exact path="/movies" component={Movies}/>
+    </Switch>
+  )
 };
 
 export default App;
