df8a1bff666c74a64523b57d7eaf2315bc2b7535
claire demont <claire.edith.demont@gmail.com>
2021-09-16 18:00:22 +0200

    change logo navbar

--- app/javascript/components/Navigation/Navbar.js
+++ app/javascript/components/Navigation/Navbar.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import friends from '../../images/friends.png';
-import watchList from '../../images/movie_blue.png';
-import popCorn from '../../images/popcorn_blue.png';
+import home from '../../images/navbar/home.svg';
+import chat from '../../images/navbar/chat.svg';
+import popCorn from '../../images/navbar/popcorn.svg';
+import user from '../../images/navbar/user.svg'
 import Button from 'react-bootstrap/Button';
 
 const Navbar = (props) => {
   return (
     <nav className="d-flex justify-content-around align-items-center">
       <Link to="/">
-        <img src={friends} alt=""/>
+        <img src={home} alt=""/>
       </Link>
-      {/* <Link to="/genres">
+      <Link to="/screenings/new">
         <img src={popCorn} alt="" />
-      </Link> */}
-      <Link to="/watch_list">
-        <img src={watchList} alt="" />
       </Link>
-      {!props.isLoggedIn &&
+      <Link to="/">
+        <img src={chat} alt="" />
+      </Link>
+      <Link to="/">
+        <img src={user} alt="" />
+      </Link>
+
+      {/* {!props.isLoggedIn &&
         <Button variant="dark">
           <Link to="/users/sign_in">Login</Link>
         </Button>
@@ -26,7 +31,7 @@ const Navbar = (props) => {
         <Button variant="dark" onClick={props.logoutHandler}>
           Sign Out
         </Button>
-      }
+      } */}
     </nav>
   );
 };
